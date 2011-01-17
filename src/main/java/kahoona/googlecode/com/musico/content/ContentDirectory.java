cb3ad7d1cfc06b3a30362c4ff929a20887014cb3
kahoona77 <kahoona77@gmail.com>
2011-01-17 16:35:13 +0100

    added contentprovider

---
+++ src/main/java/kahoona/googlecode/com/musico/content/ContentDirectory.java
@@ -0,0 +1,5 @@
+package kahoona.googlecode.com.musico.content;
+
+public class ContentDirectory extends ContentItem {
+
+}
---
+++ src/main/java/kahoona/googlecode/com/musico/content/ContentProvider.java
@@ -0,0 +1,20 @@
+package kahoona.googlecode.com.musico.content;
+
+import java.util.List;
+
+public interface ContentProvider {
+
+	public void init();
+	
+	public String getID();
+	
+	public List<ContentItem> browse(ContentDirectory directory);
+	
+	public List<ContentItem> searchTrack(String query);
+	
+	public List<ContentItem> searchArtist(String query);
+	
+	public List<ContentItem> searchAlbum(String query);
+	
+	
+}
